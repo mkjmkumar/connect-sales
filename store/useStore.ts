cdcdfc8fd16237e4b5acddd514975da7cc3e1572
Mukesh <mkjmkumar@rediffmail.com>
2024-10-05 13:34:18 +0900

    First Dreaft Version With Build Success and Login Working on SupaBase

--- store/useStore.ts
+++ store/useStore.ts
@@ -1,23 +1,13 @@
-import create from "zustand";
+import { create } from 'zustand'
 
 interface AppState {
-  leads: Lead[];
-  companies: Company[];
-  deals: Deal[];
-  users: User[];
-  addLead: (lead: Lead) => void;
-  addCompany: (company: Company) => void;
-  addDeal: (deal: Deal) => void;
-  // ... other state and actions
+  // Define your state properties here
+  // For example:
+  isLoggedIn: boolean;
+  // ... other properties
 }
 
 export const useStore = create<AppState>((set) => ({
-  leads: [],
-  companies: [],
-  deals: [],
-  users: [],
-  addLead: (lead) => set((state) => ({ leads: [...state.leads, lead] })),
-  addCompany: (company) => set((state) => ({ companies: [...state.companies, company] })),
-  addDeal: (deal) => set((state) => ({ deals: [...state.deals, deal] })),
-  // ... other state and actions
+  isLoggedIn: false,
+  // ... other initial state and actions
 }));
